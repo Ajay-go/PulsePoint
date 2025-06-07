916479901a76230793cd50dfacae0ef7332d72f9
AJAY GOUR <ajaygour3333@gmail.com>
2025-06-07 23:22:28 +0530

    some changes in database

--- pulse_point/src/App.jsx
+++ pulse_point/src/App.jsx
@@ -7,7 +7,6 @@ import Searchbar from '../components/Search_bar'
 import Customer_Tile from '../components/customer_tile'
 import Customer_slider from '../components/Customer_slider'
 import HomePage from '../pages/Home_page'
-import Speciality_page from '../pages/Speciality_page'
 import Signup_page from '../pages/Signup_page'
 import Login_page from '../pages/Login_page';
 import Profile_Page from "../pages/Profile_page";
@@ -24,7 +23,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         
-        <Route path="/speciality/:speciality" element={<Speciality_page />} />
         <Route path="/signup" element={<Signup_page />} />
         <Route path="/login" element={<Login_page />} />
         <Route path="/profile" element={<Profile_Page />} />
